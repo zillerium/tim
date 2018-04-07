320cc99c38eb81a04ad3754cb188367df1284878
jollysean <prabhatkr@gmail.com>
2018-04-08 00:33:52 +0800

    init

---
+++ params/denomination.go
@@ -0,0 +1,46 @@
+// Copyright 2017 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package params
+
+const (
+	// These are the multipliers for ether denominations.
+	// Example: To get the wei value of an amount in 'douglas', use
+	//
+	//    new(big.Int).Mul(value, big.NewInt(params.Douglas))
+	//
+	Paramanu      = 1
+	Anu      = 1e3
+	Nilve  = 1e6
+	Kolve  = 1e9
+	Chipte    = 1e12
+	Mapte   = 1e15
+	Tim    = 1e18
+	Sahastra = 1e21
+	Sankh  = 1e42
+
+	/*
+	Wei      = 1
+	Ada      = 1e3
+	Babbage  = 1e6
+	Shannon  = 1e9
+	Szabo    = 1e12
+	Finney   = 1e15
+	Ether    = 1e18
+	Einstein = 1e21
+	Douglas  = 1e42
+	*/
+)
